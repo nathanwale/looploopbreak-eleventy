0c40ecc3ea477349e15912e99c67eb8d70d2330b
nathanwale <48480798+nathanwale@users.noreply.github.com>
2021-12-14 22:30:16 +1100

    Initial commit. Was living dangerously for a while.

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = (eleventyConfig) => {
+    eleventyConfig.addCollection(
+        "site_tags",
+        (collection) => {
+            let tag_set = new Set();
+            for (item of collection.getAll()) {
+              if (item.data.tags) {
+                for (tag of item.data.tags) {
+                  tag_set.add(tag);
+                }
+              }
+            }
+            return Array.from(tag_set);
+        }
+    );
+};
